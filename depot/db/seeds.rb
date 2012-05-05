1751f77b2c5c843e3d0d8cf90cd2365f54e067b0
Calpo <calpo22@gmail.com>
2012-05-05 13:25:48 +0900

    6.2 rake db:seedが失敗していた。seeds.rbの い ち ぎ ょ う め に# encoding: utf-8 マジックコメント

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -1,19 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 # encoding: utf-8
 Product.delete_all
 Product.create(title: 'CoffeeScript',
@@ -27,7 +11,6 @@ Product.create(title: 'CoffeeScript',
       </p>},
   image_url:   'cs.jpg',    
   price: 36.00)
-# . . .
 Product.create(title: 'Programming Ruby 1.9',
   description:
     %{<p>
@@ -37,7 +20,6 @@ Product.create(title: 'Programming Ruby 1.9',
       </p>},
   image_url: 'ruby.jpg',
   price: 49.95)
-# . . .
 Product.create(title: 'Rails Test Prescriptions',
   description: 
     %{<p>
@@ -50,7 +32,6 @@ Product.create(title: 'Rails Test Prescriptions',
       </p>},
   image_url: 'rtp.jpg',
   price: 34.95)
-# . . .
 Product.create(title: 'アジャイルサムライ',
   description: 
     %{<p>
