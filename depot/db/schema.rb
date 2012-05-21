bab264d2b328bd9e369901d7b140bf04bc012037
Calpo <calpo22@gmail.com>
2012-05-21 21:34:34 +0900

    10.1 fin

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120521112523) do
+ActiveRecord::Schema.define(:version => 20120521122225) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
