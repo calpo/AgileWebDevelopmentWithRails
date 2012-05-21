f16c585396dbddc02b706ac756a5897e20e276c5
Calpo <calpo22@gmail.com>
2012-05-21 21:21:15 +0900

    10.1 p112, 独自unit test

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120510111909) do
+ActiveRecord::Schema.define(:version => 20120521112523) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120510111909) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
--- depot/test/unit/cart_test.rb
+++ depot/test/unit/cart_test.rb
@@ -1,7 +1,21 @@
+#encoding: utf-8
+
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add_product productが既に登録されている場合は量が増える" do
+    @cart = Cart.find 1
+    @item = @cart.add_product 1
+    assert_equal 2, @item.quantity
+  end
+
+  test "add_product productがまだ登録されていない場合は追加される" do
+    @cart = Cart.new(id: 99999)
+
+    # テストの前提条件 line_itemsの登録が無いことを念のため確認
+    assert_equal 0, @cart.line_items.count
+
+    @item = @cart.add_product 1
+    assert_equal 1, @item.quantity
+  end
 end
