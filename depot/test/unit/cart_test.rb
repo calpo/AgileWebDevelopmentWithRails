e5441df3dbfc4e80fa98f057f1584ca5e1ac7012
Calpo <calpo22@gmail.com>
2012-05-23 23:43:47 +0900

    All Greeeenn!!! fixtureにレコードデータ書いても上から順にidは1,2,3とはならないのね prod = products(:one) prod.id みたいにしてあげないといけない

--- depot/test/unit/cart_test.rb
+++ depot/test/unit/cart_test.rb
@@ -20,4 +20,22 @@ class CartTest < ActiveSupport::TestCase
     @item = @cart.add_product 1
     assert_equal 1, @item.quantity
   end
+
+  test "total_price カートが空の場合は0となる" do
+    @cart = carts(:empty)
+    assert_equal 0, @cart.total_price
+  end
+
+  test "total_price 複数種類個数の合計金額を取得する" do
+    @cart = carts(:empty)
+    @prod1 = products(:one)
+    @prod2 = products(:two)
+
+    @cart.add_product(@prod1.id).save!
+    @cart.add_product(@prod1.id).save!
+    @cart.add_product(@prod2.id).save!
+    @total_price = (@prod1.price * 2) + @prod2.price
+
+    assert_equal @total_price, @cart.total_price
+  end
 end
