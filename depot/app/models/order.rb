d6dbf7858a873913b082970e102f14d32b1eced2
Calpo <calpo22@gmail.com>
2012-06-04 11:27:42 +0900

    12.1 p154 テストが2つfailになってる

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,5 +1,10 @@
 #encoding: utf-8
 
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
   PAYMENT_TYPES = [ "現金", "クレジットカード", "注文書" ]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
