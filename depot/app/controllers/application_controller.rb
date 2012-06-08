b0073ef08118e0bc6812b4de044dfb270b25060d
Calpo <calpo22@gmail.com>
2012-06-08 12:28:42 +0900

    15.1 fin

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_filter :authorize
+  before_filter :set_i18n_locale_from_params
 
   protect_from_forgery
 
@@ -19,5 +20,20 @@ class ApplicationController < ActionController::Base
     unless User.find_by_id(session[:user_id])
       redirect_to login_url, notice: "ログインしてください"
     end
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
